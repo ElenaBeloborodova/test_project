e1b62364927e200b427f01479d48197b2ae29946
ElenaBeloborodova <Elena.Beloborodova@russianpost.ru>
2019-02-18 17:04:49 +0300

    Initial commit

---
+++ src/test/java/Base.java
@@ -0,0 +1,29 @@
+public class Base {
+    public static void main(String[] args) {
+        //System.out.println( "Hellow world!!!");
+
+        int a = 50;
+        int b = 15;
+        System.out.println("сумма a + b = " + (a+b));
+        float f = 1.05f;
+        float u = 1.05f;
+        System.out.println("сумма f + u = " + (f+u));
+        System.out.println("вычитание a - b = " + (a-b));
+        System.out.println("умножение a * b = " + (a*b));
+        System.out.println("умножение a * f = " + (a*f));
+        System.out.println("деление a / b = " + (a/b));
+        System.out.println("остаток a % b = " + (a%b));
+        int i = 0;
+        System.out.println("инкремент ++i = " + (++i));
+        System.out.println("инкремент --i = " + (--i));
+        System.out.println("a<b = " + (a<b));
+        System.out.println("a>b = " + (a>b));
+        System.out.println("a>=b = " + (a>=b));
+        System.out.println("a<=b = " + (a<=b));
+        System.out.println("a==b = " + (a==b));
+        System.out.println("a!=b = " + (a!=b));
+
+
+    }
+
+}
---
+++ src/test/java/InputConsol.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class InputConsol {
+    public static void main(String[] args) {
+       Scanner input = new Scanner (System.in);
+
+       String day,mouht;
+        System.out.println("Какой сегодня день ?");
+        day=input.nextLine();
+
+
+        System.out.println( "Какой месяц?");
+        mouht=input.nextLine();
+
+        String text ;
+
+        text= "сегодня - "+ day + "месяц - " + mouht;
+
+        System.out.println(text);
+
+
+
+   }
+
+}
